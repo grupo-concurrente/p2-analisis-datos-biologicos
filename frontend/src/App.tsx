1a0266d70fb81f07b02dfc39ac1611aa95114bb3
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-09-30 15:01:26 +0200

    feat: frontend template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-screen w-screen p-0 m-0'>
+      <h1 className='text-3xl font-bold underline'>
+        P2-Análisis de datos biológicos
+      </h1>
+    </div>
+  )
+}
+
+export default App
