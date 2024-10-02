fd8ba4722f65b733ba7590a779d6827157e686cf
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-02 18:55:10 +0200

    feat: User repository

---
+++ backend/src/main/java/com/umbrellacorporation/backend/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.umbrellacorporation.backend.repositories;
+
+import com.umbrellacorporation.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findUserByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.name = ?1")
+    Optional<User> findUserByName(String name);
+}
