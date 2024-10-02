cdfe2107885ec3d228f3c7b30e15321d2d16ec17
Augusto <sssdark302@gmail.com>
2024-10-02 11:40:26 +0200

    Main

---
+++ backend/src/main/java/com/BackendMain.java
@@ -0,0 +1,10 @@
+package com;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
