cef6d5656a4427df965a586e9021e25b0ef10082
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-02 23:59:10 +0200

    feat: add tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
