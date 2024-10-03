7e8e129ee831119428ed7aa8ee02084c80ebacdc
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-03 22:50:53 +0200

    feat: routing system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,70 @@
 import './App.css'
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+import Login from '@/pages/Login'
+import Landing from '@/pages/Landing'
+import Dashboard from './pages/Dashboard'
 
 function App() {
+  // Mock state for authentication
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isDataFetched, setIsDataFetched] = useState(false)
+
   return (
-    <div className='w-screen w-screen p-0 m-0'>
-      <h1 className='text-3xl font-bold underline'>
-        P2-Análisis de datos biológicos
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        {/* Ruta de Login */}
+        <Route
+          path='/login'
+          element={
+            isAuthenticated ? (
+              isDataFetched ? (
+                <Navigate to='/dashboard' replace />
+              ) : (
+                <Navigate to='/landing' replace />
+              )
+            ) : (
+              <Login setIsAuthenticated={setIsAuthenticated} />
+            )
+          }
+        />
+
+        {/* Ruta protegida de Landing */}
+        <Route
+          path='/landing'
+          element={
+            isAuthenticated ? (
+              <Landing
+                setIsAuthenticated={setIsAuthenticated}
+                setIsDataFetched={setIsDataFetched}
+              />
+            ) : (
+              <Navigate to='/login' replace />
+            )
+          }
+        />
+
+        {/* Ruta protegida de Dashboard */}
+        <Route
+          path='/dashboard'
+          element={
+            isAuthenticated && isDataFetched ? (
+              <Dashboard setIsAuthenticated={setIsAuthenticated} />
+            ) : (
+              <Navigate to='/login' replace />
+            )
+          }
+        />
+
+        {/* Redirige cualquier ruta desconocida a /login */}
+        <Route path='*' element={<Navigate to='/login' replace />} />
+      </Routes>
+    </Router>
   )
 }
 
