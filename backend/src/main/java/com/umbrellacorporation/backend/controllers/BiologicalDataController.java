354d26ab3525d644601cbd8fa94f7ea4ba814efd
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-05 19:43:53 +0200

    fix: biological data endpoint access

--- backend/src/main/java/com/umbrellacorporation/backend/controllers/BiologicalDataController.java
+++ backend/src/main/java/com/umbrellacorporation/backend/controllers/BiologicalDataController.java
@@ -5,10 +5,13 @@ import com.umbrellacorporation.backend.services.BiologicalDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller("api/v1/biological-data")
+@RestController
+@RequestMapping("api/v1/biological-data")
 public class BiologicalDataController {
 
     private final BiologicalDataService dataService;
