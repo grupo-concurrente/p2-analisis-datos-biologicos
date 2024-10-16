19b19f0bc4ffdea7399b55c8a740bdcc11c60ce6
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-16 23:50:46 +0200

    feat: svg icons fase 1

---
+++ frontend/src/components/assets/PlayButtonIcon.tsx
@@ -0,0 +1,20 @@
+export function PlayButtonIcon() {
+  return (
+    <svg
+      viewBox='0 0 100 100'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      width='100'
+      height='100'
+    >
+      <g strokeWidth='0' />
+      <g strokeLinecap='round' strokeLinejoin='round' />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M50 91.666c23.012 0 41.666-18.654 41.666-41.666S73.012 8.334 50 8.334 8.334 26.988 8.334 50 26.988 91.666 50 91.666m-5.441-25.641 19.666-11.612c3.254-1.925 3.254-6.9 0-8.825L44.559 33.976c-3.168-1.867-7.059.565-7.059 4.415v23.221c0 3.846 3.891 6.279 7.059 4.412'
+        fill='#ce69c1'
+      />
+    </svg>
+  )
+}
