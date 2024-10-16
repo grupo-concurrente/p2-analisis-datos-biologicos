7eebbb8258400012c93a92a7645c2421bf478c2e
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-16 14:32:52 +0200

    fix: refactor main routing system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
 import { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom'
-//@ts-ignore
-import { AnimatedBackground } from 'animated-backgrounds'
-import DashboardPage from './pages/DashboardPage'
-import AuthPage from './pages/AuthPage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from '@/hocs/routes/AppRoutes'
 import { UseMode } from './lib/types'
-import SelectModePage from './pages/SelectModePage'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -27,87 +18,14 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        {/* Ruta de Login */}
-        <Route
-          path='/login'
-          element={
-            isAuthenticated ? (
-              <Navigate to='/select-mode' replace />
-            ) : (
-              <>
-                <AnimatedBackground
-                  animationName='cosmicDust'
-                  style={{ opacity: 0.09 }}
-                />
-                <AuthPage authSession={authSession} />
-              </>
-            )
-          }
-        />
-
-        {/* Ruta protegida de Selección de Modo de Uso */}
-        <Route
-          path='/select-mode'
-          element={
-            isAuthenticated ? (
-              <SelectModePage logoutUser={logoutUser} setMode={setUseMode} />
-            ) : (
-              <Navigate to='/login' replace />
-            )
-          }
-        />
-
-        {/* Ruta protegida de Real Time Configuration */}
-        <Route
-          path='/real-time-configuration'
-          element={
-            isAuthenticated ? (
-              useMode === UseMode.REAL_TIME ? (
-                //TODO: implement component
-                <></>
-              ) : (
-                <Navigate to='/select-mode' replace />
-              )
-            ) : (
-              <Navigate to='/login' replace />
-            )
-          }
-        />
-
-        {/* Ruta protegida de Near Real Time Configuration */}
-        <Route
-          path='/near-real-time-configuration'
-          element={
-            isAuthenticated ? (
-              useMode === UseMode.NEAR_REAL_TIME ? (
-                //TODO: implement component
-                <></>
-              ) : (
-                <Navigate to='/select-mode' replace />
-              )
-            ) : (
-              <Navigate to='/login' replace />
-            )
-          }
-        />
-
-        {/* Ruta protegida de Dashboard */}
-        <Route
-          path='/dashboard'
-          element={
-            isAuthenticated &&
-            (isDataFetched || useMode === UseMode.MOCKING) ? (
-              <DashboardPage logoutUser={logoutUser} />
-            ) : (
-              <Navigate to='/login' replace />
-            )
-          }
-        />
-
-        {/* Redirige cualquier ruta desconocida a /login */}
-        <Route path='*' element={<Navigate to='/login' replace />} />
-      </Routes>
+      <AppRoutes
+        isAuthenticated={isAuthenticated}
+        useMode={useMode}
+        isDataFetched={isDataFetched}
+        authSession={authSession}
+        logoutUser={logoutUser}
+        setUseMode={setUseMode}
+      />
     </Router>
   )
 }
