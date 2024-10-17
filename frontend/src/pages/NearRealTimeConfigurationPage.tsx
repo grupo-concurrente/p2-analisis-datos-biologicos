b04c088c38d920e941c186bde08486b6a6531fbc
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-17 19:58:43 +0200

    fix: update props

--- frontend/src/pages/NearRealTimeConfigurationPage.tsx
+++ frontend/src/pages/NearRealTimeConfigurationPage.tsx
@@ -1,15 +1,18 @@
 import GenericPage from '@/hocs/GenericPage'
 import NearRealTimeConfiguration from '@/hocs/NearRealTimeConfiguration'
-import { BasePageProps } from '@/lib/types'
+import { BasePageProps, BiologicalData } from '@/lib/types'
+import { SetStateAction } from 'react'
 
-interface NearRealTimeConfigurationPageProps extends BasePageProps {}
+interface NearRealTimeConfigurationPageProps extends BasePageProps {
+  setData: (value: SetStateAction<BiologicalData[]>) => void
+}
 
 const NearRealTimeConfigurationPage: React.FC<
   NearRealTimeConfigurationPageProps
-> = ({ logoutUser }) => {
+> = ({ logoutUser, setData }) => {
   return (
     <GenericPage logoutUser={logoutUser}>
-      <NearRealTimeConfiguration />
+      <NearRealTimeConfiguration setData={setData} />
     </GenericPage>
   )
 }
